89d5cc855abe9dec4bc5e09de8868fa7599efaa9
dan.sarbei <dan.sarbei@CTS-SARBEI.office.cts.ad>
2017-09-28 16:52:06 +0300

    gets and sets

---
+++ src/GetsSets/MainClass.java
@@ -0,0 +1,23 @@
+package GetsSets;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+    Person ps=new Person();
+    ps.setName("Ion");
+    ps.setSecname("Popescu");
+    ps.setAge(5);
+    ps.setMar(true);
+
+
+        System.out.println("Numele introdus este: " + ps.getName() + " "+ ps.getSecname() + " varsta "+ps.getAge());
+
+
+
+    }
+
+
+}
+
+
---
+++ src/GetsSets/Person.java
@@ -0,0 +1,45 @@
+package GetsSets;
+
+public class Person {
+
+    private String name;
+    private String secname;
+    private int age;
+    private Boolean mar;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecname() {
+        return secname;
+    }
+
+    public void setSecname(String secname) {
+        this.secname = secname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Boolean getMar() {
+        return mar;
+    }
+
+    public void setMar(Boolean mar) {
+        this.mar = mar;
+    }
+
+
+
+}
