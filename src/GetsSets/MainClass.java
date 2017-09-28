fce6a85f0c8c37a8f93af44136f9bbe056e49dd9
dan.sarbei <dan.sarbei@CTS-SARBEI.office.cts.ad>
2017-09-28 16:58:44 +0300

    21.10.17

--- src/GetsSets/MainClass.java
+++ src/GetsSets/MainClass.java
@@ -5,13 +5,18 @@ public class MainClass {
     public static void main(String[] args) {
 
     Person ps=new Person();
-    ps.setName("Ion");
+    ps.setName("");
     ps.setSecname("Popescu");
     ps.setAge(5);
     ps.setMar(true);
 
+        if (ps.getMar()==true){
 
-        System.out.println("Numele introdus este: " + ps.getName() + " "+ ps.getSecname() + " varsta "+ps.getAge());
+            String stare=" castorit";
+            System.out.println("Numele introdus este: " + ps.getName() + " "+ ps.getSecname() + " varsta "+ps.getAge() + stare);
+        }
+
+
 
 
 
